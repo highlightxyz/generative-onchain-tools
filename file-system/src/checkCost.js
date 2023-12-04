a34d2ba17202a49bf445016c0196c6f235966d61
beshup <55633921+beshup@users.noreply.github.com>
2023-12-04 02:07:09 -0500

    handle non-utf8 based files (#13)

--- file-system/src/checkCost.js
+++ file-system/src/checkCost.js
@@ -16,7 +16,7 @@ const config = require("../config.json");
 const rpcConfig = require("../../chains.json");
 const L1BlockABI = require("../abi/L1Block.json").abi;
  
-const { FILE_NAME_FORMAT  } = require("./constants.js");
+const { FILE_NAME_FORMAT, UTF_BASED_FILE_EXTENSIONS } = require("./constants.js");
 const APPROX_DEPLOY_COMPUTE_PER_BYTE = BigInt(250);
 const APPROX_DEPLOY_L1_COMPUTE_OP_BASED_PER_BYTE = BigInt(17);
 const FEE_SCALAR_BASE = BigInt(1000000);
@@ -28,6 +28,10 @@ const checkCost = async (fileName, network, useLegacyGas) => {
     const filePath = resolve(__dirname, `../files/${fileName}`);
     if (!fs.existsSync(filePath)) throw new Error("File doesn't exist in file-system/files/");
     
+    // factor in size increase with base64 encoding
+    const fileNameSplit = fileName.split(".");
+    const deployCostMultiplierBPS = UTF_BASED_FILE_EXTENSIONS.includes(fileNameSplit[fileNameSplit.length - 1]) ? BigInt(10000) : BigInt(13300);
+
     const provider = ethers.getDefaultProvider(rpcConfig[network].rpc);
 
     let gasPrice;
@@ -40,7 +44,10 @@ const checkCost = async (fileName, network, useLegacyGas) => {
     
     const stats =  await fs.promises.stat(filePath);
     console.log(`File size: ${stats.size} bytes`);
-    let weiConsumed = gasPrice * APPROX_DEPLOY_COMPUTE_PER_BYTE * BigInt(stats.size);
+    if (deployCostMultiplierBPS != BigInt(10000)) {
+        console.log(`File size multiplier: ${Number(deployCostMultiplierBPS) / 10000}`);
+    }
+    let weiConsumed = (gasPrice * APPROX_DEPLOY_COMPUTE_PER_BYTE * BigInt(stats.size)) * deployCostMultiplierBPS / BigInt(10000);
     if (config.addresses[network].isOPBasedL2) {
         const l1Block = new ethers.Contract(config.addresses[network].opL1Block, L1BlockABI, provider);
         const feeScalar = await l1Block.l1FeeScalar();
@@ -54,7 +61,7 @@ const checkCost = async (fileName, network, useLegacyGas) => {
             throw new Error(`Failed to get gas fee data for ethereum (L1) using rpc ${rpcConfig["ethereum"].rpc}`);
         }
         
-        const l1WeiConsumed = l1GasPrice * APPROX_DEPLOY_L1_COMPUTE_OP_BASED_PER_BYTE * BigInt(stats.size) * feeScalar / FEE_SCALAR_BASE;
+        const l1WeiConsumed = (l1GasPrice * APPROX_DEPLOY_L1_COMPUTE_OP_BASED_PER_BYTE  * BigInt(stats.size) * feeScalar / FEE_SCALAR_BASE) * deployCostMultiplierBPS / BigInt(10000);
         weiConsumed = weiConsumed + l1WeiConsumed;
     }
     console.log(`At current gas fees, approximate cost to deploy on ${network} is ${ethers.formatEther(weiConsumed)} ETH`);
--- file-system/src/constants.js
+++ file-system/src/constants.js
@@ -4,9 +4,12 @@ const FILE_NAME_FORMAT = /^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*\.[a-zA-Z0-9]+$/;
 const MAX_FILE_CHUNK_SIZE_BYTES = 20000;
 const MAX_CHUNKS_IN_TX = 4;
 
+const UTF_BASED_FILE_EXTENSIONS = ["txt", "html", "css", "js", "json", "xml", "md", "csv"];
+
 module.exports = {
     ENV_ACCOUNT_PREFIX,
     FILE_NAME_FORMAT,
     MAX_FILE_CHUNK_SIZE_BYTES,
-    MAX_CHUNKS_IN_TX
+    MAX_CHUNKS_IN_TX,
+    UTF_BASED_FILE_EXTENSIONS
 }
